95e40b57f47bf0448b3457f4f53c7e4cf4a1d46d
Bryan Obana <bryanobana21@gmail.com>
2023-02-09 00:25:16 -0400

    Various updates
    - Updated texts, projects and alignment
    - Improved the narrow drop-down menu
    - Improved code with map() function

---
+++ src/components/Sections/ProjectsData.jsx
@@ -0,0 +1,20 @@
+const word = 'Learn more...'
+
+export const projectData = [
+  {
+    title: 'This portfolio website',
+    description:
+      "I used React.js and different React components to build this website for myself. The website's source code is kept on GitHub and is hosted by Amazon Web Services (AWS) using their Simple Storage Service (S3) and CloudFront services.",
+    url: 'https://github.com/brobana/my-portfolio-app',
+    word: 'Access the source code on GitHub...',
+    openNewTab: true,
+  },
+  {
+    title: 'Project placeholder',
+    description:
+      'Please check back later. Content being constructed.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis praesentium ab explicabo quo a provident, mollitia cum rem, consequuntur consequatur, qui incidunt accusantium non facilis quisquam illo ea totam.',
+    url: 'projects/placeholder',
+    word: word,
+    openNewTab: false,
+  },
+];
